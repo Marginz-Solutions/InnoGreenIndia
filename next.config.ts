025fa19716e86409a1f6a660db15c2f3ef5be26f
AnandhaSivam-00 <anandsugan218@gmail.com>
2026-05-18 10:08:32 +0530

    chore: Updated the build command and included image configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    position: 'top-right'
+  },
+  logging: {
+    incomingRequests: true,
+    serverFunctions: true,
+    browserToTerminal: 'error'
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fogpzfolwgyormnelsqp.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
